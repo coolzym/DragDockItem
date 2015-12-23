cd62b9f400b1d93b88b952db511b264bdfe2d437
coolzym <coolzym@aliyun.com>
2015-12-23 17:21:01 +0800

    Initial Commit

---
+++ DragDockItem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DragDockItem
+//
+//  Created by zhang minzhang min on 15/12/23.
+//  Copyright © 2015年 zhang min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DragDockItem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DragDockItem
+//
+//  Created by zhang minzhang min on 15/12/23.
+//  Copyright © 2015年 zhang min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
